cb591c201c4eb117909b6935450a1177db60ada3
Mpsk8501 <50409830+Mpsk8501@users.noreply.github.com>
2020-07-18 15:32:14 +0500

    Complete resize table (#5)

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,25 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
+import {resizer} from '@/components/table/resizer'
 
 export class Table extends ExcelComponent {
   static className = 'table'
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
+
   toHTML() {
     return createTable(18)
+  }
+
+  onClick() {
+    console.log('click')
+  }
+
+  onMousedown(event) {
+    resizer(event, this.$root)
   }
 }
